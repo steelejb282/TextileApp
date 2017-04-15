04c14487f378972588b5572f9906817aa0ad7c3f
John Steele <steelejb@mail.uc.edu>
2017-04-15 15:45:27 -0400

    Filled in limited functionality for the Run button.  Added the basics for a linked list class.

--- TextileApp/main.cs
+++ TextileApp/main.cs
@@ -5,11 +5,132 @@ namespace TextileApp
 {
     public partial class baseForm : Form
     {
+        public int numJob = 0;
+        public int numShift;
+        public int numMachine;
+        bool[] machine = new bool[5];
         public string jobVal;
         public string shiftVal;
 
         public string error1v1 = "Error: Please select A-Z.";
         public string error1v2 = "Error: Please assign a task.";
+
+        public class LinkedList
+        {
+            private class Node
+            {
+                public Object key;
+                public Object data;
+                public Node next;
+            }
+
+            private Node head;
+
+            public LinkedList()
+            {
+                head = null;
+            }
+
+            public void createNode(Object key,Object data)
+            {
+                Node newNode = new Node();
+
+                newNode.key = key;
+                newNode.data = data;
+                newNode.next = null;
+
+                addNode(newNode);
+            }
+            private void addNode(Node newNode)
+            {
+                Node curNode = new Node();
+
+                if (head == null)
+                {
+                    head = newNode;
+                    return;
+                }
+
+                curNode = head;
+                bool allow = true;
+
+                while (curNode.next != null)
+                {
+                    if (curNode.key == newNode.key)
+                    {
+                        curNode.data = newNode.data;
+                        allow = false;
+                        break;
+                    }
+                    curNode = curNode.next;
+                }
+
+                if (allow) curNode.next = newNode;
+
+                return;
+            }
+            
+            public void delNode(Object key)
+            {
+                // This linked list is designed to allow only unique values as keys
+                Node prevNode = new Node();
+                Node curNode = new Node();
+
+                // Case 1: List is empty
+                if (head == null) return;
+
+                // Case 2: head is target for deletion
+                if (head.key == key)
+                {
+                    // Relies on Automated Memory Management to care for unused objects
+                    // Case 2a: head.next == null
+                    if (head.next == null) head = null;
+                    // Case 2b: head.next != null
+                    else head = head.next;
+                }
+
+                // Case 3: Target for deletion comes after head
+                prevNode = head;
+                curNode = head.next;
+                
+                while (curNode != null)
+                {
+                    if (curNode.key == key)
+                    {
+                        prevNode.next = curNode.next;
+                        break;
+                    }
+                    else
+                    {
+                        prevNode = curNode;
+                        curNode = curNode.next;
+                    }
+                }
+
+                return;
+            }
+
+            public Object getNode(Object key)
+            {
+                Node curNode = new Node();
+
+                if (head == null) return null;
+                else
+                {
+                    curNode = head;
+
+                    while (curNode != null)
+                    {
+                        if (curNode.key == key) return curNode.data;
+                        else curNode = curNode.next;
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        LinkedList JobList = new LinkedList();
 
         public baseForm()
         {
@@ -44,14 +165,14 @@ namespace TextileApp
             }
             else
             {
-                error1.Visible = false;
                 jobVal = jobVal.ToUpper();
                 int inKey = jobVal[0];
 
                 if (inKey >= 'A' && inKey <= 'Z')
                 {
-                    inKey -= 64;
-                    outDisp.Text = inKey.ToString();
+                    inKey -= 64; // inKey -= 'A' + 1: Converts char to int starting at 1
+                    outDisp.Text = inKey.ToString(); // Debugging in place of action
+                    error1.Visible = false;
                 }
                 else
                 {
@@ -65,11 +186,60 @@ namespace TextileApp
 
         private void runBut_Click(object sender, EventArgs e)
         {
+            bool[] pass = new bool[3];
 
             // Step 1: Check Job List
+            pass[0] = true;
             // Step 2: Check Shifts
+            shiftVal = shiftSel.Text;
+
+            if (shiftVal.Length == 1)
+            {
+                int inShift = shiftVal[0];
+
+                if (inShift >= '2' && inShift <= '5')
+                {
+                    numShift = inShift - '2' + 2;
+                    pass[1] = true;
+                }
+                else
+                {
+                    shiftSel.Text = "";
+                    pass[1] = false;
+                }
+            }
+            else pass[1] = false;
+
             // Step 3: Check Machines
+            numMachine = 0;
+            pass[2] = false;
+
+            for (int i=0;i<machineList.Items.Count;i++)
+            {
+                if (machineList.GetItemCheckState(i) == CheckState.Checked)
+                {
+                    machine[i] = true;
+                    numMachine++;
+                }
+                else machine[i] = false;
+            }
+
+            if (numMachine > 0) pass[2] = true;
+
             // Step 4: Do Stuff
+
+            if (pass[0]) error1.Visible = false;
+            else
+            {
+                error1.Text = error1v2;
+                error1.Visible = true;
+            }
+
+            if (pass[1]) error2.Visible = false;
+            else error2.Visible = true;
+
+            if (pass[2]) error3.Visible = false;
+            else error3.Visible = true;
         }
     }
 }
